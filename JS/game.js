a3afbe942179654e4d2123c11ee9ae2f55e07161
Adam Eliasson <adam.eliasson.swe@gmail.com>
2023-06-13 14:29:26 +0200

    in progress on finish the first game

--- JS/game.js
+++ JS/game.js
@@ -3,18 +3,13 @@ const boardGame = document.querySelector(".boardGame");
 let currentPlayer = 1;
 let player1Count = 0;
 let player2Count = 0;
-let columnWinner = 0;
+
 document.getElementById("menu").addEventListener("click", () => {
     window.location = `./`
 })
-
-document.querySelector("#playerone").classList.add("currentPlayer");
-// if(currentPlayer === 1){
-//     document.querySelector("#playertwo").classList.remove("currentPlayer");
-// }else {
-//     document.querySelector("#playertwo").classList.add("currentPlayer");
-//     document.querySelector("#playerone").classList.remove("currentPlayer");
-// }
+document.getElementById("restart").addEventListener("click", () => {
+    console.log("hello");
+})
 
 let datasetId = 1;
 for (let row = 1; row < 7; row++) {
@@ -27,78 +22,82 @@ for (let row = 1; row < 7; row++) {
         divSlot.style.gridArea = `${row} / ${column}`;
         datasetId++;
     }
-    
+
 }
 
 for (let i = 0; i < 7; i++) {
     let slotButton = document.createElement("div");
-        slotButton.classList.add("slotButtons");
-        slotButton.dataset.slotDecider = i;
-        slotButton.addEventListener("click", fillSlot);
-        document.querySelector(".boardHeader").append(slotButton);
-
-    
+    slotButton.classList.add("slotButtons");
+    slotButton.dataset.slotDecider = i;
+    slotButton.addEventListener("click", fillSlot);
+    document.querySelector(".boardHeader").append(slotButton);
 }
 
-console.log(boardGame);
-
-function fillSlot (event) {
+function fillSlot(event) {
     let slotColumn = event.currentTarget.dataset["slotDecider"];
     let slot = slotsArray[slotColumn][0];
-    if(slotsArray[slotColumn].length === 0){
+    if (slotsArray[slotColumn].length === 0) {
         return;
     }
     const correctSlot = document.querySelector(`[data-slot-id="${slot}"]`);
     correctSlot.id = `player${currentPlayer}`;
     correctSlot.classList.remove(`empty`);
+    correctSlot.classList.add(`filled`);
     slotsArray[slotColumn].splice(0, 1);
-    // if(currentPlayer === 1){
-    // }else {
-    // }
-    if(currentPlayer === 1){
-        currentPlayer = 2;
-        document.querySelector("#playertwo").classList.add("currentPlayer");
-        document.querySelector("#playerone").classList.remove("currentPlayer");
-    }else {
-        document.querySelector("#playerone").classList.add("currentPlayer");
-        document.querySelector("#playertwo").classList.remove("currentPlayer");
-        currentPlayer = 1;
-    }
-    
-    checkWinner();
+
+    currentPlayer === 1 ? currentPlayer = 2 : currentPlayer = 1;
+    document.querySelector("#playerone").classList.toggle("currentPlayer");
+    document.querySelector("#playertwo").classList.toggle("currentPlayer");
+
+    controllBoardGameWinner();
 }
 
-function checkWinner () {
+function controllBoardGameWinner() {
+
+    // Controlles the rows ____________________________________
     for (let row = 6; row > 0; row--) {
         for (let column = 1; column < 8; column++) {
             let element = document.querySelector(`[style="grid-area: ${row} / ${column};"]`);
-            if(element.classList.contains("empty")){
+            if (element.classList.contains("empty")) {
                 player1Count = 0;
                 player2Count = 0;
                 continue
             }
-            checkRowWinner(element);
-            
+            possibleWin(element);
         }
-        
     }
-    
+
+    // Controlles the columns ____________________________________
+    for (let column = 1; column < 8; column++) {
+        for (let row = 6; row > 0; row--) {
+            let element = document.querySelector(`[style="grid-area: ${row} / ${column};"]`);
+            if (element.classList.contains("empty")) {
+                player1Count = 0;
+                player2Count = 0;
+                continue
+            }
+            possibleWin(element);
+        }
+    }
 }
 
-function checkRowWinner (element) {
-    let id = element.id;
-    if(id === "player1"){
+function possibleWin(element) {
+    const currentPlayer = element.id;
+    const otherPlayer = currentPlayer === "player1" ? "playertwo" : "playerone";
+    let emoji = "";
+    if (currentPlayer === "player1") {
+        emoji = "playerone";
         player1Count++;
         player2Count = 0
     }
-    if(id === "player2"){
+    if (currentPlayer === "player2") {
+        emoji = "playertwo";
         player2Count++;
         player1Count = 0
     }
-    if(player1Count === 4) {
-        console.log(id + " is the winner");
-    } 
-    if (player2Count === 4){
-        console.log(id + " is the winner");
+    if (player1Count === 4 || player2Count === 4) {
+        document.querySelector(`#${emoji}`).classList.add("winner");
+        document.querySelector(`#${otherPlayer}`).classList.add("loser");
+        console.log(currentPlayer + " is the winner");
     }
 }
