b4abeeaf4946fb4f8b6117077b49a694dc63b2b7
Adam Eliasson <adam.eliasson.swe@gmail.com>
2023-09-13 14:04:38 +0200

    de

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 
 
+// window.addEventListener("load", function () {
+
+//     setTimeout(function () { window.scrollTo(0, 1); }, 100);
+
+// })
+
 window.addEventListener("load", function () {
 
-    setTimeout(function () { window.scrollTo(0, 1); }, 100);
+    if (document.height <= window.outerHeight) {
+
+        document.body.style.height = (window.outerHeight + 50) + 'px';
+
+        setTimeout(function () { window.scrollTo(0, 1); }, 50);
+
+    }
+
+    else {
+
+        setTimeout(function () { window.scrollTo(0, 1); }, 0);
+
+    }
 
 })
+
 
 
 
